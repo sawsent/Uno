1bdbf4c1bc67daa9da091b8a802659d33453d2e5
sawsent <vicenteppfigueiredo@gmail.com>
2024-03-01 19:26:47 +0000

    fixing merge v5

--- src/io/codeforall/bootcamp/filhosdamain/uno/Main.java
+++ src/io/codeforall/bootcamp/filhosdamain/uno/Main.java
@@ -1,7 +1,29 @@
 package io.codeforall.bootcamp.filhosdamain.uno;
 
+import io.codeforall.bootcamp.filhosdamain.uno.server.Server;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            new Server(getPort());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int getPort() {
+        while (true) {
+            System.out.print("Port?\n>> ");
+            try {
+                return (new Scanner(System.in)).nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("That's not a valid port Integer");
+            }
+        }
     }
 }
