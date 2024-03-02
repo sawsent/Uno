8f8f539479b93d69f7b9ab073b95f0a184817127
Tuna <zetuna.23@gmail.com>
2024-03-02 22:12:23 +0000

    message ready

--- src/io/codeforall/bootcamp/filhosdamain/uno/messages/ShowTurnSkipped.java
+++ src/io/codeforall/bootcamp/filhosdamain/uno/messages/ShowTurnSkipped.java
@@ -1,16 +1,32 @@
 package io.codeforall.bootcamp.filhosdamain.uno.messages;
 
+import io.codeforall.bootcamp.filhosdamain.uno.game.Color;
 import io.codeforall.bootcamp.filhosdamain.uno.game.Player;
 import io.codeforall.bootcamp.filhosdamain.uno.game.PlayerList;
 
+import java.util.LinkedList;
+
 public class ShowTurnSkipped implements Message {
 
+    private final LinkedList<Player> players = new LinkedList<>();
+    private final Player player;
+    private final String mode;
+
     public ShowTurnSkipped(PlayerList players, Player player) {
-
-
+        this.players.addAll(players);
+        this.player = player;
+        this.mode = "all";
     }
     @Override
     public void send() {
-
+        if (mode.equals("all")) {
+            for (Player p : players) {
+                if (p.equals(player)) {
+                    continue;
+                }
+                p.getPrintStream().print(Color.GREEN.ANSI_CODE + player.getName() + "'s turn has been skipped!" + Color.RESET);
+            }
+        }
+        player.getPrintStream().print(Color.RED.ANSI_CODE + "Your turn has been skipped!!" + Color.RESET);
     }
-}
+    }
